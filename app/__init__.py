5842eb3840d039cc6706d29e166083799ed52c65
Isabel Zheng <isabelz13@nycstudents.net>
2025-12-11 01:08:24 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -103,9 +103,17 @@ def trivia():
 
     return render_template("trivia.html", username=session['username'], trivia=trivia_data)
 
-@app.route("/jokes")
-def jokes():
-    return render_template("jokes.html")
+def get_joke():
+    url = "https://v2.jokeapi.dev/joke/Programming?blacklistFlags=religious,political,racist,sexist"
+    data = get_data (url)
+    return data   
+        
+@app.route("/jokes") 
+def jokes(): 
+    joke_db = get_joke()
+    if (joke_db == url_err):
+        return render_template ("keyerror.html", API = "Jokes API", err = joke_data)
+    return render_template("jokes.html", username = session['username'], joke = joke_db)
 
 @app.route("/activites")
 def activities():
