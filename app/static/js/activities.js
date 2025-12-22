8bd3417657b7f2c29f690b1aab4271ed526915de
Jason Chan <jchan61@stuy.edu>
2025-12-22 07:00:37 -0500

    headings

--- app/static/js/activities.js
+++ app/static/js/activities.js
@@ -1,3 +1,9 @@
+// Squash (Jason Chan, Aoanul Hoque, Isabel Zheng, Maya Berchin)
+// SoftDev
+// P01: ArRESTed Development
+// 2025-12-22
+// time spent: 2 hours
+
 function init(n, p, a) {
 	var num_val_options = n;
 	var price_options = p;
@@ -7,34 +13,34 @@ function init(n, p, a) {
 	var num_output = document.getElementById("ppl");
 	var num_cur = Number(num_slider.value);
 	num_output.innerHTML = num_val_options[num_cur]; // Display the default slider value
-					
+
 	// Update the current slider value (each time you drag the slider handle)
 	num_slider.oninput = function() {
 		var ind = Number(this.value);
 		num_output.innerHTML = num_val_options[ind];
-	} 
-				
-					
+	}
+
+
 	var prate_slider = document.getElementById("price");
 	var prate_output = document.getElementById("prate");
 	var prate_cur = Number(prate_slider.value);
 	prate_output.innerHTML = price_options[prate_cur]; // Display the default slider value
-					
+
 	// Update the current slider value (each time you drag the slider handle)
 	prate_slider.oninput = function() {
 		var ind = Number(this.value);
 		prate_output.innerHTML = price_options[ind];
-	} 
-					
+	}
+
 
 	var acc_slider = document.getElementById("accessibility");
 	var acc_output = document.getElementById("acc");
 	var acc_cur = Number(acc_slider.value);
 	acc_output.innerHTML = accessibility_options[acc_cur]; // Display the default slider value
-					
+
 	// Update the current slider value (each time you drag the slider handle)
 	acc_slider.oninput = function() {
 		var ind = Number(this.value);
 		acc_output.innerHTML = accessibility_options[ind];
-	} 
+	}
 }
